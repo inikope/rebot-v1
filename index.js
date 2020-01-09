ce3ee249d1f898172988dfe90b6fc4fa07e29e65
inikope <59614495+inikope@users.noreply.github.com>
2020-01-09 20:34:35 +0700

    Working Bioig

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.get('/', (req, res) => {
                 console.log("Full Name: " + fullName);
                 console.log("Bio Instagram: " +igbio);
                 console.log("Link Instagram: " +iglink);
-                const sendBio = "Nama: "+ fullName +"\nBio:\n"+ igbio + "\nPosts: "+ values[2].data +"\nFollowers: "+ values[3].data +"\nFollowing: "+ values[4].data +"\nLink: "+ iglink;
+                const sendBio = "𝐍𝐚𝐦𝐚: "+ fullName +"\n𝐁𝐢𝐨:\n"+ igbio + "\n𝐏𝐨𝐬𝐭𝐬: "+ values[2].data +"\n𝐅𝐨𝐥𝐥𝐨𝐰𝐞𝐫𝐬: "+ values[3].data +"\n𝐅𝐨𝐥𝐥𝐨𝐰𝐢𝐧𝐠: "+ values[4].data +"\n𝐋𝐢𝐧𝐤: "+ iglink;
                 return replyText(token, sendBio);    
             })
     }
