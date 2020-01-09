880964de76beca559c66477ec3b9153c834ef206
inikope <59614495+inikope@users.noreply.github.com>
2020-01-09 15:54:12 +0700

    Fixed Kembali (asdadfa)

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get('/', (req, res) => {
 
 // IG Get functions
     function getIG(what, igid){
-        var kembali = "";
+        var kembali;
 	    switch(what){
 		    case 'fullName':
             	kembali = instaProf.getFullname(igid).then(res => {
@@ -60,20 +60,20 @@ app.get('/', (req, res) => {
                 return user;
             });break;
             case 'post':
-            	kembali = String(instaProf.getPosts(igid).then(res => {
+            	kembali = instaProf.getPosts(igid).then(res => {
         	    const user = res.data;
                 return user;
-            }));break;
+            });break;
             case 'follower':
-            	kembali = String(instaProf.getFollowers(igid).then(res => {
+            	kembali = instaProf.getFollowers(igid).then(res => {
         	    const user = res.data;
                 return user;
-            }));break;
+            });break;
             case 'following':
-            	kembali = String(instaProf.getFollowing(igid).then(res => {
+            	kembali = instaProf.getFollowing(igid).then(res => {
 	            const user = res.data;
                 return user;
-            }));break;
+            });break;
             case 'picprev':
             	kembali = instaProf.instaRegular(igid).then(res => {
 	            const user = res.data;
