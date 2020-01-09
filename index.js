739d4465613d4c1bbfff9b6d47c642cfcf08e5d2
inikope <59614495+inikope@users.noreply.github.com>
2020-01-09 19:39:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -44,61 +44,6 @@ app.get('/', (req, res) => {
 
     //Bio IG
     function bioIG(token, igid){
-        // let fullName;
-        // let igbio;
-        // let igpost;
-        // let igfollower;
-        // let igfollowing;
-        // let iglink;
-
-        // const storeName = (result) => {
-        //     new Promise(function(resolve, reject) {
-        //         if(result.data === 'false'){
-        //             fullName = '-';
-        //         } else {
-        //             fullName = result.data;
-        //         }    
-        //         resolve(fullName);
-        //     })
-        // }
-        // const storeBio = (result) => {
-        //     new Promise(function(resolve, reject) {
-        //         if(result.data === 'false'){
-        //         igbio = '-';
-        //     } else {
-        //         igbio = result.data;
-        //     }
-        //     resolve(igbio);
-        //     })
-        // }
-        // const storePosts = (result) => {
-        //     new Promise(function(resolve, reject) {
-        //         igpost = result.data;
-        //         resolve(igpost);
-        //     })
-        // }
-        // const storeFollower = (result) => {
-        //     new Promise(function(resolve, reject) {
-        //         igfollower = result.data;
-        //         resolve(igfollower);
-        //     })
-        // }
-        // const storeFollowing = (result) => {
-        //     new Promise(function(resolve, reject) {
-        //         igfollowing = result.data;
-        //         resolve(igfollowing);
-        //     })
-        // }
-        // const storeLink = (result) => {
-        //     new Promise(function(resolve, reject) {
-        //         if(result.data === 'false'){
-        //         iglink = '-';
-        //     } else {
-        //         iglink = result.data;
-        //     }
-        //     resolve(iglink);
-        //     })
-        // }
             const p1 = instaProf.getFullname(igid);
             const p2 = instaProf.getBio(igid);
             const p3 = instaProf.getPosts(igid);
@@ -107,16 +52,13 @@ app.get('/', (req, res) => {
             const p6 = instaProf.getExternalUrl(igid);
             Promise.all([p1,p2,p3,p4,p5,p6]).then(function(values){
                 console.log(values);
-                if(values[1].data === false){
-                    values[1].data = '-';
-                }
-                if(values[2].data === false){
-                    values[2].data = '-';
-                }
-                if(values[6].data === false){
-                    values[6].data = '-';
-                }
-                const sendBio = "Nama: "+ values[1].data +"\nBio:\n"+ values[2].data + "\nPosts: "+ values[3].data +"\nFollowers: "+ values[4].data +"\nFollowing: "+ values[5].data +"\nLink: "+ values[6].data;
+                const fullName = (values[1].data)? values[1].data : '-';
+                const igbio = (values[2].data)? values[2].data : '-';
+                const iglink = (values[6].data)? values[6].data : '-';
+                console.log("Full Name: " + fullName);
+                console.log("Bio Instagram: " +igbio);
+                console.log("Link Instagram: " +iglink);
+                const sendBio = "Nama: "+ fullName +"\nBio:\n"+ igbio + "\nPosts: "+ values[3].data +"\nFollowers: "+ values[4].data +"\nFollowing: "+ values[5].data +"\nLink: "+ iglink;
                 return replyText(token, sendBio);    
             })
     }
